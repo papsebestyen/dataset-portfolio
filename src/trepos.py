d402df9b21da90df9d7c397ea9d37ef9a821c9ca
papsebestyen <pap.sebestyen@gmail.com>
2021-09-23 00:46:47 +0200

    modify subsets and trepos

--- src/trepos.py
+++ src/trepos.py
@@ -1,4 +1,4 @@
 from sscutils import create_trepo_with_subsets
 
-comments_table = create_trepo_with_subsets("comments", group_cols='topic')
+comments_table = create_trepo_with_subsets("comments")
 users_table = create_trepo_with_subsets("users")
