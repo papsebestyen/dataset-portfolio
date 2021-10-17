6ff1513135d3746a8aa5b034134f899643590695
papsebestyen <pap.sebestyen@gmail.com>
2021-10-17 13:48:16 +0200

    Modify subset creation

--- src/create_subsets.py
+++ src/create_subsets.py
@@ -15,6 +15,8 @@ def create_subsets(subset_name, only_hun_bluechip = False):
     
     only_hun_bluechip: bool -> Only a subset of hungarian bluechip stocks
     """
+    if subset_name == "complete":
+        return
     comments_df = comments_table.get_full_df().loc[lambda df: df['topic_id'].isin(blue_chip_ids.values()) & only_hun_bluechip]
     users_df = users_table.get_full_df()
 
